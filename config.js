f2bf90a796d5dc27f6d651bcc4c87f7217e86d31
siddhigate12 <117962421+siddhigate12@users.noreply.github.com>
2023-12-21 09:39:57 +0530

    Update config.js

--- config.js
+++ config.js
@@ -18,7 +18,7 @@ module.exports = {
             }
         },
         sidepanel: {
-            showSidePanel: true
+            showSidePanel: false
         },
         content: {},
         footer: {},
